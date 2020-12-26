8236ebce10afc50e1f0e7b9f923a1df234593fe2
davisantosr <davi-rocha2@hotmail.com>
2020-12-25 23:43:35 -0300

    add validation | express-validator

--- server/routes/houses.js
+++ server/routes/houses.js
@@ -1,10 +1,21 @@
 const express = require('express');
+const { check, validationResult } = require('express-validator')
 
 const House = require('../models/House')
 
 const router = express.Router();
 
-router.post('/', (req, res) => {
+router.post('/', [
+  check('title').isLength({min: 3, max: 50})
+  .withMessage('Title should have 3 to 50 characters')
+], (req, res) => {
+
+  const errors = validationResult(req)
+
+  if(!errors.isEmpty()) {
+    return res.status(422).send({'errors': errors.array()})
+  }
+
   const house = new House({
     title: req.body.title,
     address: req.body.address, 

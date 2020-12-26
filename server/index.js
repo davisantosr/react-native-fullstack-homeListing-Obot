575a2309c631b9ac54f5022e44e3b8bcd66ba3a3
davisantosr <davi-rocha2@hotmail.com>
2020-12-25 21:38:18 -0300

    add post request to create a new type

--- server/index.js
+++ server/index.js
@@ -3,6 +3,8 @@ require('dotenv').config()
 
 const app = express();
 
+app.use(express.json())
+
 const homes = [
   {
     id:1, 
@@ -32,6 +34,19 @@ app.get('/api/listing/:id', (req, res) => {
     res.status(404).send('The home with the given id was not found')
   }
   res.send(home);
+})
+
+
+app.post('/api/listing', (req, res) => {
+  const home = {
+    id: homes.length + 1,
+    type: req.body.type, 
+    description: req.body.description 
+  }
+
+  homes.push(home);
+  res.send(home);
+
 })
 
 const port = process.env.PORT || 3000;

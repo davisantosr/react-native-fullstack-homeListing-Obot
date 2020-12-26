5b07bf31163e585e6fbaa0d2d230640108393f0f
davisantosr <davi-rocha2@hotmail.com>
2020-12-25 22:29:42 -0300

    add mongoose | connect db mongodb

--- server/index.js
+++ server/index.js
@@ -1,73 +1,20 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
 require('dotenv').config()
+
 
 const app = express();
 
 app.use(express.json())
 
-const homes = [
-  {
-    id:1, 
-    type:'apartment', 
-    description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nam minima amet rem?'
-  },
-  {
-    id:2, 
-    type:'flat', 
-    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam soluta enim totam ut dolor magnam?'
-  } 
-
-]
-
-app.get('/', (req, res) => {
-  res.send('Welcome')
-})
-
-app.get('/api/listing', (req, res) => {
-  res.send(homes)
-})
-
-app.get('/api/listing/:id', (req, res) => {
-  const home = homes.find(home => home.id ===  parseInt(req.params.id))
-  
-  if(!home) {
-    res.status(404).send('The home with the given id was not found')
-  }
-  res.send(home);
-})
-
-
-app.post('/api/listing', (req, res) => {
-
-  if(!req.body.type || !req.body.description) {
-    return res.status(400).send('Title and Description is required')
-  }
-
-  const home = {
-    id: homes.length + 1,
-    type: req.body.type, 
-    description: req.body.description 
-  }
-
-  homes.push(home);
-  res.send(home);
-
-})
-
-app.put('/api/listing/:id', (req, res) => {
-  const home = homes.find(home => home.id === parseInt(req.params.id));
-
-  if(!home) {
-    return res.status(404).send('The home with the given id was not found')
-  }
-
-  home.type = req.body.type;
-  home.description = req.body.description;
-
-  res.send(home)
-  
-})
-
 const port = process.env.PORT || 3000;
 
-app.listen(port, () => console.log(`Server is running on port ${port}`))
+mongoose.connect(`mongodb+srv://home-listing-user:${process.env.DB_PASSWORD}@cluster0.tbrmp.mongodb.net/house_app?retryWrites=true&w=majority`)
+  .then(result => {
+    
+    app.listen(port, () => console.log(`Server is running on port ${port}`))
+
+  })
+  .catch(err => console.log(err))
+

aa5cfb07677a987f0cc39c7bd61455c13b167844
davisantosr <davi-rocha2@hotmail.com>
2020-12-26 15:12:45 -0300

    add AddHomeScreen styling

--- client/src/screens/AddHomeScreen.js
+++ client/src/screens/AddHomeScreen.js
@@ -1,17 +1,77 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, ScrollView, TextInput, Button } from 'react-native';
 
 
 const AddHomeScreen = () => {
   return (
-    <View>
-      <Text>AddHomeScreen</Text>
-    </View>
+    <ScrollView>
+      <View style={styles.form}>
+        <View style={styles.formGroup}>
+          <Text style={styles.label}>Title</Text>
+          <TextInput style={styles.input} />
+        </View>
+        <View style={styles.formGroup}>
+          <Text style={styles.label}>ImageUrl</Text>
+          <TextInput style={styles.input} />
+        </View>
+        <View style={styles.formGroup}>
+          <Text style={styles.label}>Type</Text>
+          <TextInput style={styles.input} />
+        </View>
+        <View style={styles.formGroup}>
+          <Text style={styles.label}>Price</Text>
+          <TextInput style={styles.input} />
+        </View>
+        <View style={styles.formGroup}>
+          <Text style={styles.label}>Year Build</Text>
+          <TextInput style={styles.input} />
+        </View>
+        <View style={styles.formGroup}>
+          <Text style={styles.label}>Address</Text>
+          <TextInput 
+            style={styles.input} 
+            multiline
+          />
+        </View>
+        <View style={styles.formGroup}>
+          <Text style={styles.label}>Description</Text>
+          <TextInput 
+            style={styles.input} 
+            multiline
+          />
+        </View>
+        <View style={styles.buttonContainer}>
+          <Button
+            title='Add Home'  
+          />
+        </View>
+      </View>
+    </ScrollView>
   )
 }
 
 const styles = StyleSheet.create({
-
+  form: {
+    margin: 20,
+    backgroundColor: '#ffffff',
+    padding: 20, 
+    borderRadius: 10
+  },
+  formGroup: {
+    width: '100%'
+  },
+  label:{
+    marginVertical: 10
+  },
+  input: {
+    paddingHorizontal: 2, 
+    paddingVertical: 8,
+    borderBottomColor: '#ccc', 
+    borderBottomWidth: 1
+  }, 
+  buttonContainer: {
+    marginTop: 20
+  }
 });
 
 export default AddHomeScreen

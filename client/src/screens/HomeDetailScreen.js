c72221b4b44cd92807dd34763d280f16874b80d5
davisantosr <davi-rocha2@hotmail.com>
2020-12-26 15:33:54 -0300

    add HomeDetails styling

--- client/src/screens/HomeDetailScreen.js
+++ client/src/screens/HomeDetailScreen.js
@@ -5,7 +5,7 @@ import { View, Text, StyleSheet, ScrollView, Image } from 'react-native';
 const HomeDetailScreen = () => {
   return (
     <ScrollView>
-      <View style={styles.contaiener}>
+      <View style={styles.container}>
         <View style={styles.heading}>
           <Text style={styles.title}>Modern House</Text>
         </View>
@@ -17,23 +17,23 @@ const HomeDetailScreen = () => {
         </View>
         <View style={styles.group}>
           <Text style={styles.label}>Home Type:</Text>
-          <Text styles={styles.value}>Flat</Text>
+          <Text style={styles.value}>Flat</Text>
         </View>
         <View style={styles.group}>
           <Text style={styles.label}>Price:</Text>
-          <Text styles={styles.value}>$2.000.000,00</Text>
+          <Text style={styles.value}>$2.000.000,00</Text>
         </View>
         <View style={styles.group}>
           <Text style={styles.label}>Year Build:</Text>
-          <Text styles={styles.value}>2020</Text>
+          <Text style={styles.value}>2020</Text>
         </View>
         <View style={styles.group}>
           <Text style={styles.label}>Address:</Text>
-          <Text styles={styles.value}>This is some address</Text>
+          <Text style={styles.value}>This is some address</Text>
         </View>
         <View style={styles.group}>
           <Text style={styles.label}>Decription:</Text>
-          <Text styles={styles.value}>This is some description</Text>
+          <Text style={styles.value}>This is some description</Text>
         </View>
 
       </View>
@@ -42,7 +42,35 @@ const HomeDetailScreen = () => {
 }
 
 const styles = StyleSheet.create({
-  
+  container: {
+    flex: 1,
+    marginVertical: 20, 
+  }, 
+  heading: {
+    marginHorizontal: 20, 
+    marginBottom: 10,
+  },
+  title: {
+    fontSize: 24,
+  },
+  image: {
+    width: '100%', 
+    height: 200
+  },
+  group: {
+    marginHorizontal: 20,
+    marginVertical: 10,
+    flexDirection: 'row',
+    alignItems: 'center'
+  },
+  label: {
+    fontSize: 18
+  },
+  value: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    flexShrink: 1
+  }
 });
 
 export default HomeDetailScreen

d19122dee369e9bd77ca55da690bf4fa2a3dcfcd
davisantosr <davi-rocha2@hotmail.com>
2020-12-26 15:22:57 -0300

    add HomeDetails structure

--- client/src/screens/HomeDetailScreen.js
+++ client/src/screens/HomeDetailScreen.js
@@ -1,17 +1,48 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, ScrollView, Image } from 'react-native';
 
 
 const HomeDetailScreen = () => {
   return (
-    <View>
-      <Text>HomeDetailScreen</Text>
-    </View>
+    <ScrollView>
+      <View style={styles.contaiener}>
+        <View style={styles.heading}>
+          <Text style={styles.title}>Modern House</Text>
+        </View>
+        <View>
+          <Image
+            style={styles.image}
+            source={require('../../assets/images/house.jpg')}
+          />
+        </View>
+        <View style={styles.group}>
+          <Text style={styles.label}>Home Type:</Text>
+          <Text styles={styles.value}>Flat</Text>
+        </View>
+        <View style={styles.group}>
+          <Text style={styles.label}>Price:</Text>
+          <Text styles={styles.value}>$2.000.000,00</Text>
+        </View>
+        <View style={styles.group}>
+          <Text style={styles.label}>Year Build:</Text>
+          <Text styles={styles.value}>2020</Text>
+        </View>
+        <View style={styles.group}>
+          <Text style={styles.label}>Address:</Text>
+          <Text styles={styles.value}>This is some address</Text>
+        </View>
+        <View style={styles.group}>
+          <Text style={styles.label}>Decription:</Text>
+          <Text styles={styles.value}>This is some description</Text>
+        </View>
+
+      </View>
+    </ScrollView>
   )
 }
 
 const styles = StyleSheet.create({
-
+  
 });
 
 export default HomeDetailScreen
